a049055c1a5baad97293da2651a21a7a01f8c6e5
Anand Akella <akella_anand@yahoo.com>
2025-07-08 11:40:32 -0400

    Style active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import { link } from 'fs'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { BsEnvelopePaperHeartFill } from 'react-icons/bs'
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: 'Home', href: '/'},
     { label: 'About Us', href: '/about'},
@@ -18,7 +24,11 @@ const NavBar = () => {
           {links.map(link => 
           <Link 
           key={link.href} 
-          className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+          className={classnames({
+            'text-zinc-900': link.href === currentPath,
+            'text-zinc-500': link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors': true
+          })} 
           href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
